5d9c344357168da04b41cd558111bcfbb6a8f8d2
Sylvain MONTEILLET <monteillet.s@mipih.fr>
2015-12-09 10:54:58 +0100

    Rename test class

--- src/test/java/DiamondTest.java
+++ src/test/java/DiamondTest.java
@@ -8,7 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Created by monteillet on 07/12/2015.
  */
-public class DiamondGeneratorTest {
+public class DiamondTest
+{
 
 
     @Test
